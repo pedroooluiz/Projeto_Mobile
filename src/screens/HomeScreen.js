30fbb3bc4bcc5b50347fd421361e699e445cf5bb
Pedro Luiz <105122745+pedroooluiz@users.noreply.github.com>
2023-11-22 15:31:52 -0300

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, StyleSheet } from 'react-native';
 import { Card } from 'react-native-paper';
 import Api from '../services/Api';
 
@@ -7,7 +7,6 @@ function HomeScreen({ navigation }) {
   const [muscles, setMuscles] = useState([]);
 
   useEffect(() => {
-    // Fazer a requisição à API e atualizar o estado 'muscles' com os dados
     Api.get('/exercises')
       .then((response) => {
         const muscleData = response.data.map((exercise) => exercise.muscle);
@@ -16,19 +15,19 @@ function HomeScreen({ navigation }) {
       })
       .catch((error) => {
         console.error('Erro ao obter dados da API:', error);
-        // Isso é para checar sse a Api estava retornado um array
       });
-  }, []); 
+  }, []);
 
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Home Screen</Text>
 
       <FlatList
         data={muscles}
         keyExtractor={(item) => item}
         renderItem={({ item }) => (
           <Card
+            style={styles.card}
             onPress={() => {
               navigation.navigate('Treinos', { muscle: item });
             }}
@@ -40,5 +39,25 @@ function HomeScreen({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#E2DFE6',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#4285F4', // Cor azul, você pode ajustar conforme necessário
+  },
+  card: {
+    marginBottom: 16,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: 'white',
+  },
+});
 
 export default HomeScreen;
